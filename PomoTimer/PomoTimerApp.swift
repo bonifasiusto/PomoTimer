41e02b39f5b2615ad479a112528c5708d5a76293
bonifasiusto <bonifasius_nifa77@yahoo.com>
2022-07-22 00:27:50 +0700

    Initial Commit

---
+++ PomoTimer/PomoTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoTimerApp.swift
+//  PomoTimer
+//
+//  Created by Bonifasius Toto Neguisa Ginting on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct PomoTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
